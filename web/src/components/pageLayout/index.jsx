b94afa6c33cd0462465acb7c6d2fe96c6c9b46df
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2024-02-01 17:31:44 -0300

    developing product page card [under construction]

--- web/src/components/pageLayout/index.jsx
+++ web/src/components/pageLayout/index.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ProductCards from '../productsCards';
 import HeaderSection from '../headerSection';
 import HeaderApp from '../headerApp';
+import { ApiContext } from '../../contexts/ApiContext';
 
 function LayoutPage({ children }) {
+  const { products } = useContext(ApiContext);
+
+  let plataforma = products.map(item => {
+    return item.platform;
+  })
+
+  const novaArr = plataforma.filter((este, i) => {
+    return plataforma.indexOf(este) === i;
+  });
+
+  let valor = 0;
+  let valor2 = 0;
+
+  products.map(item => {
+    if(item.platform === "Mercado Livre"){
+      valor = item.saleprice;
+    }else{
+      valor2 = item.saleprice;
+    }
+  })
+
+  console.log(valor)
+  console.log(valor2)
+
+
   return (
     <section className="mt-2 rounded-lg bg-secundary-white dark:bg-secundary-blue lg:mt-0 lg:w-[73%] xl:w-full">
       <HeaderApp />
@@ -12,10 +38,7 @@ function LayoutPage({ children }) {
         <HeaderSection title="Produtos" subtitle="Registrar produtos" />
         <main>
           <div className="mt-10 flex flex-col justify-between gap-2 md:grid md:grid-cols-2 2xl:flex 2xl:flex-row">
-            <ProductCards platform="Plataforma 1" />
-            <ProductCards platform="Plataforma 2" />
-            <ProductCards platform="Plataforma 3" />
-            <ProductCards platform="Plataforma 4" />
+            <ProductCards platform={novaArr} valor={valor} valor2={valor2}/>
           </div>
           <div className="mt-5">
             {children}
--- web/src/components/productsCards/index.jsx
+++ web/src/components/productsCards/index.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiSolidBarChartAlt2 } from 'react-icons/bi';
 import { IoIosArrowDropupCircle } from 'react-icons/io';
 import PropTypes from 'prop-types';
 
-function ProductCards({ platform }) {
+function ProductCards({ platform, valor,valor2 }) {
+  const [distance, setDistance] = useState("");
+
+  console.log(platform)
+
+
   return (
     <div className="flex w-full flex-col gap-3 rounded-lg bg-white p-5 dark:bg-primary-blue">
       <header className="flex justify-between">
-        <h3 className="dark:text-white/70">{platform}</h3>
+        <select className='dark:text-white/70' onChange={(e) => setDistance({selecionado: e.target.value})}>
+          {platform.map((item) => (
+            <option key={item} value={item} className='text-black'>{item}</option>
+          ))}
+        </select>
         <span className=" rounded-full bg-primary-white p-2 text-secundary-gray/80 drop-shadow-lg dark:bg-secundary-blue">
           <BiSolidBarChartAlt2 />
         </span>
@@ -15,13 +24,11 @@ function ProductCards({ platform }) {
       <main className="flex items-center justify-between">
         <div>
           <p className="text-[11px] text-sm dark:text-white/50">Vendas</p>
-          <h1 className="text-2xl font-medium dark:text-white">R$ 34,500</h1>
+          <h1 className="text-2xl font-medium dark:text-white">R$ {distance.selecionado === 'Americanas' ? valor : valor2}</h1>
         </div>
         <div>
           <p className="text-[11px] dark:text-white/50">Lucro</p>
-          <h1 className="text-2xl font-medium dark:text-white md:pr-9">
-            R$ 34,500
-          </h1>
+          <h1 className="text-2xl font-medium dark:text-white md:pr-9">R$ 1000</h1>
         </div>
       </main>
       <footer className="mt-2 flex items-center justify-between gap-2">
@@ -51,5 +58,5 @@ function ProductCards({ platform }) {
 export default ProductCards;
 
 ProductCards.propTypes = {
-  platform: PropTypes.string.isRequired,
+  platform: PropTypes.node,
 };
