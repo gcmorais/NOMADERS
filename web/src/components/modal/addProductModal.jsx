1710278558015fa9e34653359a111c7422351f4f
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2024-02-21 16:54:18 -0300

    feat: add existing products to list

--- web/src/components/modal/addProductModal.jsx
+++ web/src/components/modal/addProductModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Login from "./Portal";
 import Input from "../input/input";
@@ -6,10 +6,45 @@ import List from "../list";
 import Select from "../input/select";
 import useErrors from "../../hooks/useErrors";
 import FormGroup from "../input/formgroup";
+import { ApiContext } from "../../contexts/ApiContext";
+import ProductsServices from "../../services/ProductsService";
+import { DateMask } from "../../utils/DateMask";
 
 function AddProductModal({ isOpen, setModalOpen }) {
   const [checked, setChecked] = React.useState([]);
+  const [test, setTest] = React.useState([]);
   const [date, setDate] = useState("");
+  const [distance, setDistance] = useState({});
+
+  const { loadProducts } = useContext(ApiContext);
+  const { products } = useContext(ApiContext);
+
+  async function handleSubmit(formData) {
+    formData.preventDefault();
+    try {
+      const data = {
+        name: checked[0].name,
+        ean: checked[0].ean,
+        platform: distance.selecionado,
+        cost: checked[0].cost,
+        salePrice: checked[0].saleprice,
+        dateValue: date,
+      };
+      const response = await ProductsServices.createProduct(data);
+      loadProducts();
+      console.log(response);
+    } catch (error) {
+      console.log("Ocorreu um erro, tente novamente mais tarde.", error);
+    }
+  }
+
+  let plataforma = products.map((item) => {
+    return item.platform;
+  });
+
+  const novaArr = plataforma.filter((este, i) => {
+    return plataforma.indexOf(este) === i;
+  });
 
   const { setError, removeError, getErrorMessageByFieldName, errors } =
     useErrors();
@@ -17,16 +52,13 @@ function AddProductModal({ isOpen, setModalOpen }) {
   const isFormValid = date && errors.length === 0;
 
   function handleDateChange(event) {
-    setDate(event.target.value);
+    setDate(DateMask(event.target.value));
 
     if (!event.target.value) {
       setError({ field: "data", message: "Data é obrigatória." });
     } else {
       removeError("data");
     }
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
   }
 
   if (isOpen) {
@@ -46,66 +78,85 @@ function AddProductModal({ isOpen, setModalOpen }) {
             <List checked={checked} setChecked={setChecked} />
           </div>
           <div className="flex flex-col gap-5">
-            <div>
-              {checked.map((item) => {
-                return (
-                  <div key={item.id}>
-                    <header className="mb-5 text-xl dark:text-primary-white">
-                      <h1>{item.name}</h1>
-                    </header>
-                    <main className="flex flex-col gap-2 ">
-                      <div>
-                        <p className="text-primary-gray dark:text-primary-white/60">
-                          EAN
-                        </p>
-                        <span className="text-black/80 dark:text-primary-white">
-                          {item.ean}
-                        </span>
-                      </div>
-                      <div className="flex justify-between">
-                        <div>
-                          <p className="text-primary-gray dark:text-primary-white/60">
-                            Custo
-                          </p>
-                          <span className="text-black/80 dark:text-primary-white">
-                            R$ {item.cost}
-                          </span>
-                        </div>
-                        <div>
-                          <p className="text-primary-gray dark:text-primary-white/60">
-                            Preço de venda
-                          </p>
-                          <span className="text-black/80 dark:text-primary-white">
-                            R$ {item.saleprice}
-                          </span>
-                        </div>
-                        <div>
-                          <p className="text-primary-gray dark:text-primary-white/60">
-                            Lucro
-                          </p>
-                          <span className="text-green-600">
-                            R$ {item.saleprice - item.cost}
-                          </span>
-                        </div>
-                      </div>
-                    </main>
-                  </div>
-                );
-              })}
-            </div>
             <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+              <div>
+                {checked.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <header className="mb-5 text-xl dark:text-primary-white">
+                        <h1>{item.name}</h1>
+                      </header>
+                      <main className="flex flex-col gap-2 ">
+                        <div>
+                          <p className="text-primary-gray dark:text-primary-white/60">
+                            EAN
+                          </p>
+                          <span className="text-black/80 dark:text-primary-white">
+                            {item.ean}
+                          </span>
+                        </div>
+                        <div className="flex justify-between">
+                          <div>
+                            <p className="text-primary-gray dark:text-primary-white/60">
+                              Custo
+                            </p>
+                            <span className="text-black/80 dark:text-primary-white">
+                              R$ {item.cost}
+                            </span>
+                          </div>
+                          <div>
+                            <p className="text-primary-gray dark:text-primary-white/60">
+                              Preço de venda
+                            </p>
+                            <span className="text-black/80 dark:text-primary-white">
+                              R$ {item.saleprice}
+                            </span>
+                          </div>
+                          <div>
+                            <p className="text-primary-gray dark:text-primary-white/60">
+                              Lucro
+                            </p>
+                            <span className="text-green-600">
+                              R$ {item.saleprice - item.cost}
+                            </span>
+                          </div>
+                        </div>
+                      </main>
+                    </div>
+                  );
+                })}
+              </div>
               <div className="flex gap-5">
-                <FormGroup error={getErrorMessageByFieldName("data")}>
+                <FormGroup error={getErrorMessageByFieldName("date-value")}>
                   <Input
                     text="Data de venda"
                     styles="dark:text-white"
-                    value={date}
+                    value={date.substr(0, 20)}
                     change={handleDateChange}
+                    date={true}
+                    valueMask={10}
                   />
                 </FormGroup>
-
                 <FormGroup styles="w-full">
-                  <Select title="Plataforma" value="Mercado livre" />
+                  <p className="font-inter text-sm font-semibold opacity-70 dark:text-white">
+                    Plataforma
+                  </p>
+                  <select
+                    className="w-full rounded-lg border-2 border-primary-gray/30 p-2 text-sm opacity-70 dark:bg-primary-blue dark:text-white/50 "
+                    onChange={(e) =>
+                      setDistance({ selecionado: e.target.value })
+                    }
+                  >
+                    {novaArr.map((item) => (
+                      <option
+                        key={item}
+                        value={item}
+                        className="text-black dark:text-white/50 "
+                      >
+                        {item}
+                      </option>
+                    ))}
+                  </select>
                 </FormGroup>
               </div>
               <div className="flex flex-row-reverse">
--- web/src/components/productForm/index.jsx
+++ web/src/components/productForm/index.jsx
@@ -1,128 +1,136 @@
-import React, {
-  useState, forwardRef, useImperativeHandle,
-} from 'react';
-import PropTypes from 'prop-types';
-import Input from '../input/input';
-import useErrors from '../../hooks/useErrors';
-import FormGroup from '../input/formgroup';
-import { DateMask } from '../../utils/DateMask';
+import React, { useState, forwardRef, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
+import Input from "../input/input";
+import useErrors from "../../hooks/useErrors";
+import FormGroup from "../input/formgroup";
+import { DateMask } from "../../utils/DateMask";
 import { useNavigate } from "react-router-dom";
 
-
 const ProductForm = forwardRef(({ title, btnLabel, onSubmit, edit }, ref) => {
-  const [name, setName] = useState('');
-  const [ean, setEan] = useState('');
-  const [platform, setPlatform] = useState('');
-  const [cost, setCost] = useState('');
-  const [salePrice, setSalePrice] = useState('');
-  const [dateValue, setDateValue] = useState('');
+  const [name, setName] = useState("");
+  const [ean, setEan] = useState("");
+  const [platform, setPlatform] = useState("");
+  const [cost, setCost] = useState("");
+  const [salePrice, setSalePrice] = useState("");
+  const [dateValue, setDateValue] = useState("");
 
   const navigate = useNavigate();
 
-  const {
-    setError, removeError, getErrorMessageByFieldName, errors,
-  } = useErrors();
+  const { setError, removeError, getErrorMessageByFieldName, errors } =
+    useErrors();
 
-  const isFormValid = (name && ean && platform && cost && salePrice && errors.length === 0);
+  const isFormValid =
+    name && ean && platform && cost && salePrice && errors.length === 0;
 
-  useImperativeHandle(ref, () => ({
-    setFieldsValues: (data) => {
-      setName(data.name);
-      setEan(data.ean);
-      setPlatform(data.platform);
-      setCost(data.cost);
-      setSalePrice(data.saleprice);
-      setDateValue(data.datevalue);
-    },
-  }), []);
-  
+  useImperativeHandle(
+    ref,
+    () => ({
+      setFieldsValues: (data) => {
+        setName(data.name);
+        setEan(data.ean);
+        setPlatform(data.platform);
+        setCost(data.cost);
+        setSalePrice(data.saleprice);
+        setDateValue(data.datevalue);
+      },
+    }),
+    [],
+  );
+
   const autoNavigate = () => {
     navigate("/app/nomaders/products");
-  }
-  
+  };
 
   function handleNameChange(event) {
     setName(event.target.value);
 
     if (!event.target.value) {
-      setError({ field: 'name', message: 'Nome é obrigatório!' });
+      setError({ field: "name", message: "Nome é obrigatório!" });
     } else {
-      removeError('name');
+      removeError("name");
     }
   }
   function handleEanChange(event) {
     setEan(event.target.value);
 
     if (!event.target.value) {
-      setError({ field: 'ean', message: 'Ean é obrigatório!' });
+      setError({ field: "ean", message: "Ean é obrigatório!" });
     } else {
-      removeError('ean');
+      removeError("ean");
     }
   }
   function handlePlatformChange(event) {
     setPlatform(event.target.value);
 
     if (!event.target.value) {
-      setError({ field: 'platform', message: 'Plataforma é obrigatória!' });
+      setError({ field: "platform", message: "Plataforma é obrigatória!" });
     } else {
-      removeError('platform');
+      removeError("platform");
     }
   }
   function handleCostChange(event) {
     setCost(event.target.value);
 
     if (!event.target.value) {
-      setError({ field: 'cost', message: 'Insira o custo do produto' });
+      setError({ field: "cost", message: "Insira o custo do produto" });
     } else {
-      removeError('cost');
+      removeError("cost");
     }
   }
   function handleSalePriceChange(event) {
     setSalePrice(event.target.value);
 
     if (!event.target.value) {
-      setError({ field: 'sale-price', message: 'Insira o valor de venda do produto' });
+      setError({
+        field: "sale-price",
+        message: "Insira o valor de venda do produto",
+      });
     } else {
-      removeError('sale-price');
+      removeError("sale-price");
     }
   }
   function handleDateValueChange(event) {
     setDateValue(DateMask(event.target.value));
 
     if (!event.target.value) {
-      setError({ field: 'date-value', message: 'Insira uma data válida' });
+      setError({ field: "date-value", message: "Insira uma data válida" });
     } else {
-      removeError('date-value');
+      removeError("date-value");
     }
   }
   async function handleSubmit(event) {
     event.preventDefault();
 
     await onSubmit({
-      name, ean, platform, cost, salePrice, dateValue,
+      name,
+      ean,
+      platform,
+      cost,
+      salePrice,
+      dateValue,
     });
 
-    if(edit){
+    if (edit) {
       autoNavigate();
     }
 
-    setName('');
-    setEan('');
-    setPlatform('');
-    setCost('');
-    setSalePrice('');
-    setDateValue('');
+    setName("");
+    setEan("");
+    setPlatform("");
+    setCost("");
+    setSalePrice("");
+    setDateValue("");
   }
   return (
     <>
       <header className="mb-9 flex flex-col gap-8">
-        <h1 className="font-inter text-2xl font-semibold m-auto dark:text-white">
+        <h1 className="m-auto font-inter text-2xl font-semibold dark:text-white">
           {title}
         </h1>
       </header>
       <main>
         <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-          <FormGroup error={getErrorMessageByFieldName('name')}>
+          <FormGroup error={getErrorMessageByFieldName("name")}>
             <Input
               text="Nome"
               styles="dark:text-white"
@@ -132,7 +140,7 @@ const ProductForm = forwardRef(({ title, btnLabel, onSubmit, edit }, ref) => {
             />
           </FormGroup>
 
-          <FormGroup error={getErrorMessageByFieldName('ean')}>
+          <FormGroup error={getErrorMessageByFieldName("ean")}>
             <Input
               text="Ean"
               styles="dark:text-white"
@@ -141,8 +149,8 @@ const ProductForm = forwardRef(({ title, btnLabel, onSubmit, edit }, ref) => {
               valueMask={13}
             />
           </FormGroup>
-          
-          <FormGroup error={getErrorMessageByFieldName('platform')}>
+
+          <FormGroup error={getErrorMessageByFieldName("platform")}>
             <Input
               text="Plataforma"
               styles="dark:text-white"
@@ -152,7 +160,7 @@ const ProductForm = forwardRef(({ title, btnLabel, onSubmit, edit }, ref) => {
             />
           </FormGroup>
           <div className="flex gap-5">
-            <FormGroup error={getErrorMessageByFieldName('cost')}>
+            <FormGroup error={getErrorMessageByFieldName("cost")}>
               <Input
                 text="Custo"
                 placeholder="R$"
@@ -163,7 +171,7 @@ const ProductForm = forwardRef(({ title, btnLabel, onSubmit, edit }, ref) => {
               />
             </FormGroup>
 
-            <FormGroup error={getErrorMessageByFieldName('sale-price')}>
+            <FormGroup error={getErrorMessageByFieldName("sale-price")}>
               <Input
                 text="Preço de venda"
                 placeholder="R$"
@@ -173,7 +181,7 @@ const ProductForm = forwardRef(({ title, btnLabel, onSubmit, edit }, ref) => {
                 valueMask={4}
               />
             </FormGroup>
-            <FormGroup error={getErrorMessageByFieldName('date-value')}>
+            <FormGroup error={getErrorMessageByFieldName("date-value")}>
               <Input
                 text="Data de venda"
                 styles="dark:text-white"
@@ -187,16 +195,17 @@ const ProductForm = forwardRef(({ title, btnLabel, onSubmit, edit }, ref) => {
           <button
             type="submit"
             disabled={!isFormValid}
-            className={isFormValid
-              ? 'px-13 m-auto mb-10 mt-2 flex w-full items-center justify-center rounded-md bg-primary-indigo p-2 text-primary-white'
-              : 'px-13 m-auto mb-10 mt-2 flex w-full items-center justify-center rounded-md bg-primary-indigo/50 p-2 text-primary-white'}
+            className={
+              isFormValid
+                ? "px-13 m-auto mb-10 mt-2 flex w-full items-center justify-center rounded-md bg-primary-indigo p-2 text-primary-white"
+                : "px-13 m-auto mb-10 mt-2 flex w-full items-center justify-center rounded-md bg-primary-indigo/50 p-2 text-primary-white"
+            }
           >
             {btnLabel}
           </button>
         </form>
       </main>
     </>
-
   );
 }, []);
 
