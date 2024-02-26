8b2f0a8a5f955678a24272d13a90f763c69210b9
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2024-02-26 13:59:20 -0300

    deploy configs

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,17 @@
-const express = require('express');
-require('express-async-errors');
+const express = require("express");
+require("express-async-errors");
 
-const routes = require('./routes');
-const cors = require('./app/middlewares/cors');
-const errorHandler = require('./app/middlewares/errorHandler');
+const routes = require("./routes");
+const cors = require("./app/middlewares/cors");
+const errorHandler = require("./app/middlewares/errorHandler");
 
 const app = express();
+
+const port = process.env.PORT || 5174;
 
 app.use(express.json());
 app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(5174, () => console.log('Server started at http://localhost:5174'));
+app.listen(port, () => console.log("Server started at http://localhost:5174"));
