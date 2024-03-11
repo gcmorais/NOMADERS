30237aaa8b147599bbaa21901a1998628cd46882
Guilherme Morais <guilhermeclarodemorais@gmail.com>
2024-03-11 13:27:34 -0300

    feat: change api adress

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = new axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://api-nomaders.onrender.com/",
   headers: {
     Authorization: localStorage.getItem("@Auth:token"),
   },
